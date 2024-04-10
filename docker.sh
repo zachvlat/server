e65ec6fdc558e66ea83a85be38bb2f660b7ee4fa
Zacharias Vlatakis <zachariasvlatakis@gmail.com>
2024-04-10 16:38:00 +0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -8,11 +8,12 @@ fi
 
 echo "Installing proper dependencies and repos..."
 
-sudo apt -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+sudo apt update
+sudo apt -y install apt-transport-https ca-certificates curl gnupg software-properties-common
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-echo "deb [arch=arm64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt update
 
